c9ad96bb9b7ec1e98abab55a0b62c5793cb22fb3
1000ch <shogo.sensui@gmail.com>
2016-02-23 01:46:20 +0900

    Revert code styles

--- spec/linter-htmlhint-spec.js
+++ spec/linter-htmlhint-spec.js
@@ -8,42 +8,34 @@ describe('The htmlhint provider for Linter', () => {
   beforeEach(() => {
     atom.workspace.destroyActivePaneItem();
     waitsForPromise(() =>
-      atom
-        .packages
-        .activatePackage('linter-htmlhint')
-        .then(() => atom.packages.activatePackage('language-html'))
+      Promise.all([
+        atom.packages.activatePackage('linter-htmlhint'),
+        atom.packages.activatePackage('language-html')
+      ])
     );
   });
 
   it('detects invalid coding style in bad.html and report as error', () => {
     waitsForPromise(() => {
       const bad = path.join(__dirname, 'fixtures', 'bad.html');
-      return atom
-        .workspace
-        .open(bad)
-        .then(editor => lint(editor))
-        .then(messages => {
-          expect(messages.length).toEqual(1);
+      return atom.workspace.open(bad).then(editor => lint(editor)).then(messages => {
+        expect(messages.length).toEqual(1);
 
-          // test only the first error
-          expect(messages[0].type).toEqual('error');
-          expect(messages[0].text).toEqual('Doctype must be declared first.');
-          expect(messages[0].filePath).toMatch(/.+bad\.html$/);
-          expect(messages[0].range).toEqual([[0, 0], [0, 13]]);
-        });
+        // test only the first error
+        expect(messages[0].type).toEqual('error');
+        expect(messages[0].text).toEqual('Doctype must be declared first.');
+        expect(messages[0].filePath).toMatch(/.+bad\.html$/);
+        expect(messages[0].range).toEqual([[0, 0], [0, 13]]);
+      });
     });
   });
 
   it('finds nothing wrong with a valid file (good.html)', () => {
     waitsForPromise(() => {
       const good = path.join(__dirname, 'fixtures', 'good.html');
-      return atom
-        .workspace
-        .open(good)
-        .then(editor => lint(editor))
-        .then(messages => {
-          expect(messages.length).toEqual(0);
-        });
+      return atom.workspace.open(good).then(editor => lint(editor)).then(messages => {
+        expect(messages.length).toEqual(0);
+      });
     });
   });
 });
