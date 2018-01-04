8c47790bb0fd84d4372a9a9977e1075ef3bb69cf
Eric Cornelissen <ericornelissen@gmail.com>
2018-01-04 22:12:32 +0100

    Implement @Arcanemagus suggestions

--- spec/linter-htmlhint-spec.js
+++ spec/linter-htmlhint-spec.js
@@ -7,6 +7,9 @@ import { it, fit, wait, beforeEach, afterEach } from 'jasmine-fix';
 const { lint } = require('../lib/index.js').provideLinter();
 
 describe('The htmlhint provider for Linter', () => {
+  const badFile = path.join(__dirname, 'fixtures', 'bad.html');
+  const goodFile = path.join(__dirname, 'fixtures', 'good.html');
+
   beforeEach(async () => {
     atom.workspace.destroyActivePaneItem();
     await atom.packages.activatePackage('linter-htmlhint');
@@ -14,44 +17,42 @@ describe('The htmlhint provider for Linter', () => {
   });
 
   it('detects invalid coding style in bad.html and report as error', async () => {
-    const bad = path.join(__dirname, 'fixtures', 'bad.html');
-    const editor = await atom.workspace.open(bad);
+    const editor = await atom.workspace.open(badFile);
     const messages = await lint(editor);
 
     expect(messages.length).toEqual(1);
     expect(messages[0].type).toBe('error');
     expect(messages[0].text).toBe('Doctype must be declared first.');
-    expect(messages[0].filePath).toBe(bad);
+    expect(messages[0].filePath).toBe(badFile);
     expect(messages[0].range).toEqual([[0, 0], [0, 5]]);
   });
 
   it('finds nothing wrong with a valid file (good.html)', async () => {
-    const good = path.join(__dirname, 'fixtures', 'good.html');
-    const editor = await atom.workspace.open(good);
+    const editor = await atom.workspace.open(goodFile);
     const messages = await lint(editor);
 
     expect(messages.length).toBe(0);
   });
 
-  it('lints an invalid file (bad.html) with a default configuration when no config file is present', async () => {
-    atom.config.set('linter-htmlhint.disableWhenNoHtmhintConfig', false);
+  describe('The "Disable when no HTMLHint config is found" option', () => {
+    it('lints files with no config when disabled', async () => {
+      atom.config.set('linter-htmlhint.disableWhenNoHtmhintConfig', false);
 
-    const bad = path.join(__dirname, 'fixtures', 'bad.html');
-    const editor = await atom.workspace.open(bad);
-    spyOn(editor, 'getPath').andReturn(__dirname);
-    const messages = await lint(editor);
+      const editor = await atom.workspace.open(badFile);
+      spyOn(editor, 'getPath').andReturn(__dirname);
+      const messages = await lint(editor);
 
-    expect(messages.length).toEqual(3);
-  });
+      expect(messages.length).toEqual(3);
+    });
 
-  it('does not lint an invalid file (bad.html) when no config is found and the disable option set', async () => {
-    atom.config.set('linter-htmlhint.disableWhenNoHtmhintConfig', true);
+    it("doesn't lint files with no config when enabled", async () => {
+      atom.config.set('linter-htmlhint.disableWhenNoHtmhintConfig', true);
 
-    const bad = path.join(__dirname, 'fixtures', 'bad.html');
-    const editor = await atom.workspace.open(bad);
-    spyOn(editor, 'getPath').andReturn(__dirname);
-    const messages = await lint(editor);
+      const editor = await atom.workspace.open(badFile);
+      spyOn(editor, 'getPath').andReturn(__dirname);
+      const messages = await lint(editor);
 
-    expect(messages).toBe(null);
+      expect(messages).toBe(null);
+    });
   });
 });
